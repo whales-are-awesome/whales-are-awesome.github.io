7f8114de9498f5cd613bb041b34afe702990b056
Nikita <suponov.nickita@gmail.com>
2023-09-30 02:57:47 +0300

    twekas

---
+++ assets/link-65e87c9f.js
@@ -0,0 +1,4 @@
+const t=`<svg xmlns="http://www.w3.org/2000/svg" width="13" height="12" viewBox="0 0 13 12" fill="none">
+    <path d="M1.5 11L11.5 1M11.5 1H2.40909M11.5 1V10.0909" stroke="#FFA753"/>
+</svg>
+`;export{t as default};
