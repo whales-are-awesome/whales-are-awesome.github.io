4e1234c4627cc45b52720e6698a8045d01dad039
Nikita <suponov.nickita@gmail.com>
2024-02-16 17:24:34 +0300

    some

--- assets/link-65e87c9f.js
+++ assets/link-65e87c9f.js
@@ -2,4 +2,3 @@ const t=`<svg xmlns="http://www.w3.org/2000/svg" width="13" height="12" viewBox=
     <path d="M1.5 11L11.5 1M11.5 1H2.40909M11.5 1V10.0909" stroke="#FFA753"/>
 </svg>
 `;export{t as default};
-//# sourceMappingURL=link-65e87c9f.js.map
