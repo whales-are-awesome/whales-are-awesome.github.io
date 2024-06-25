eff2189186d754f91f4ab6faae286f2b42f2b874
Nikita <suponov.nickita@gmail.com>
2024-06-25 22:22:15 +0300

    tweaks

--- assets/index-14ac99e3.js
+++ assets/index-14ac99e3.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-4185b93c.js";import"./index-7d08978c.js";/**
+import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-193664b9.js";import"./index-217a47ab.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
