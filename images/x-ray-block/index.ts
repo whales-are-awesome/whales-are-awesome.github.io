d1a026b80236ab488a51e49b695fab45192975b7
Nikita <suponov.nickita@gmail.com>
2024-07-01 16:14:50 +0300

    tweaks

---
+++ images/x-ray-block/index.ts
@@ -0,0 +1,6 @@
+export { default as sliderImage } from './slider.png?inline';
+export { default as dotImage } from '../common/dot.png?inline';
+export { default as shipImage } from './ship.png?inline';
+export { default as shipMobileImage } from './ship-mobile.png?inline';
+export { default as shipXRayImage } from './ship-xray.png?inline';
+export { default as shipXRayMobileImage } from './ship-xray-mobile.png?inline';
